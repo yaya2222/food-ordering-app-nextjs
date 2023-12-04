8a36abe1a2e44e3e017c6e7219db62cd4c8f8032
יעקב גולדמן <gyaakov2000@gmail.com>
2023-12-05 00:05:14 +0200

    Add Header and Hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
-    <div>
-test
-    </div>
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import React from 'react'
+import Right from '../icons/Right'
+
+export default function Hero() {
+    return (
+        <section className='hero'>
+            <div className='py-12'>
+                <h1 className='text-4xl font-semibold'>Everything<br />is better<br />with a&nbsp;
+                    <span className='text-primary'>Pizza</span></h1>
+                <p className='my-4 text-gray-500 text-sm'>Pizza is the missing piecs that makes every day complete, a simple yet delicious joy in life</p>
+                <div className='flex gap-4'>
+                    <button className='flex items-center gap-2 uppercase bg-primary text-white text-sm rounded-full px-4 py-2'>Order now <Right /></button>
+                    <button className='flex gap-2 py-2 text-gray-600 font-semibold'>Learn more <Right /></button>
+                </div>
+            </div>
+            <div className='relative'>
+                <Image src="/pizza.png" alt='pizza' layout='fill' objectFit='contain' />
+            </div>
+        </section>
+    )
+}
