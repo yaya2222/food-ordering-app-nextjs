e8f3a7327aa195516fb54d79a3d905067afb0120
Yakov <yakov@tapuzi.tapuzi>
2023-12-07 08:08:10 +0200

    work on page register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function RegisterPage() {
   return (
-    <section className='text-center text-primary text-4xl mt-8'>
-        <h1>Register</h1>
-        <form>
-            <input type='email' placeholder='email' />
-            <input type='password' placeholder='password' />
-            <button type='submit'>Register</button>
-        </form>
+    <section className=" mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      <form className="block max-w-xs mx-auto">
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Register</button>
+        <div className="my-4 text-center text-gray-500">or login with provider</div>
+        <button>Login with google</button>
+      </form>
     </section>
-  )
+  );
 }
